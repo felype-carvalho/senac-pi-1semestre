f3f96ea89b7b95916c336ce689a89998af0ac23c
Hsilva68 <henrique16868@hotmail.com>
2020-11-20 23:53:22 -0300

    Alimentar vetores
    Alimentei os vetores de pertuntas e respostas

--- src/code/Main.java
+++ src/code/Main.java
@@ -29,17 +29,17 @@ public class Main {
 
 		alimentaVetores();
 
-		System.out.println("PI – O jogo da imitação\n");
+		System.out.println("PI â€“ O jogo da imitaÃ§Ã£o\n");
 		System.out.println(
-				"Espero que esteja ansioso para um projeto ultrassecreto, com o objetivo de tentar quebrar o indecifrável \n"
-						+ "código nazista, feito pela maquina enigma, vencer a guerra e trazer paz novamente! \n");
-		System.out.println("Matemático(a), para começarmos, por favor, diga-me o seu nome:");
+				"Espero que esteja ansioso para um projeto ultrassecreto, com o objetivo de tentar quebrar o indecifrÃ¡vel \n"
+						+ "cÃ³digo nazista, feito pela maquina enigma, vencer a guerra e trazer paz novamente! \n");
+		System.out.println("MatemÃ¡tico(a), para comeÃ§armos, por favor, diga-me o seu nome:");
 		nomeJogador = entrada.next();
 
 		System.out.println("Seja bem-vindo. \n");
 
 		do {
-			System.out.println(nomeJogador + ", está com papel e caneta na mãos? Digite abaixo:");
+			System.out.println(nomeJogador + ", estÃ¡ com papel e caneta na mÃ£os? Digite abaixo:");
 			papelCaneta = entrada.next();
 
 			switch (papelCaneta) {
@@ -53,41 +53,41 @@ public class Main {
 				confirmaCaneta = true;
 				break;
 			default:
-				System.out.println("\nAinda não está com o papel e caneta em mãos? A gente espera sem problema.");
+				System.out.println("\nAinda nÃ£o estÃ¡ com o papel e caneta em mÃ£os? A gente espera sem problema.");
 				break;
 			}
 
 		} while (confirmaCaneta == false);
 
-		System.out.println("\nEntão bora lá " + nomeJogador + "...\n"
-				+ "\nAntes de começar o projeto para decifrar o enigma, precisamos voltar no tempo e relembrar alguns conceitos que serão essenciais para decifrarmos os códigos...\n"
-				+ "Você precisa nos provar que realmente está capacitado para esse projeto, com isso vamos começar relembrando algumas coisas do ensino médio.\n"
-				+ "\nPergunta: Em que classe de grupo você ficava?");
-		System.out.println("1: Os estudiosos\n" + "2: Turma do fundão\n" + "3: Matadores de Aula\n"
+		System.out.println("\nEntÃ£o bora lÃ¡ " + nomeJogador + "...\n"
+				+ "\nAntes de comeÃ§ar o projeto para decifrar o enigma, precisamos voltar no tempo e relembrar alguns conceitos que serÃ£o essenciais para decifrarmos os cÃ³digos...\n"
+				+ "VocÃª precisa nos provar que realmente estÃ¡ capacitado para esse projeto, com isso vamos comeÃ§ar relembrando algumas coisas do ensino mÃ©dio.\n"
+				+ "\nPergunta: Em que classe de grupo vocÃª ficava?");
+		System.out.println("1: Os estudiosos\n" + "2: Turma do fundÃ£o\n" + "3: Matadores de Aula\n"
 				+ "4: Os copiadores\n");
 		do {
 			
-			 System.out.println("Digite a opção escolhida abaixo: ");
+			 System.out.println("Digite a opÃ§Ã£o escolhida abaixo: ");
 			 aluno = entrada.next();
 			 
 			switch (aluno) {
 			case "1":
-				System.out.println("\nParabéns, então você consegue nos ajudar nesse projeto!");
+				System.out.println("\nParabÃ©ns, entÃ£o vocÃª consegue nos ajudar nesse projeto!");
 				confirmaAluno = true;
 				break;
 
 			case "2":
-				System.out.println("\nVocê terá um pouco mais de dificuldade, mas lhe daremos uma chance!");
+				System.out.println("\nVocÃª terÃ¡ um pouco mais de dificuldade, mas lhe daremos uma chance!");
 				confirmaAluno = true;
 				break;
 
 			case "3":
-				System.out.println("\nPuxa, infelizmente você está dispensado do projeto");
+				System.out.println("\nPuxa, infelizmente vocÃª estÃ¡ dispensado do projeto");
 				opSair();
 				
 
 			case "4":
-				System.out.println("\nvocê terá algumas dificuldades ao longo do jogo, mas lhe daremos uma chance");
+				System.out.println("\nvocÃª terÃ¡ algumas dificuldades ao longo do jogo, mas lhe daremos uma chance");
 				confirmaAluno = true;
 				break;
 
@@ -103,85 +103,85 @@ public class Main {
 
 	static void alimentaVetores() {
 		
-		// Vetor com os textos da História
-		historia[0] = "Antes de começar o projeto para decifrar o enigma, precisamos voltar no tempo e relembrar alguns conceitos que serão essenciais para decifrarmos os códigos...";
-		historia[1] = "Você precisa nos provar que realmente está capacitado para esse projeto, com isso vamos começar relembrando algumas coisas do ensino médio";
-		historia[2] = "Agora que você faz parte do projeto ultra secreto, vamos por a mão na massa!\r\n" + 
-				"Muitas vidas já foram perdidas com essa guerra, estamos correndo contra o tempo para salvar o maior número de pessoas e vencer a guerra.\r\n" + 
-				"Temos que começar resolvendo essa primeira questão, vamos lá...\r\n" + 
+		// Vetor com os textos da HistÃ³ria
+		historia[0] = "Antes de comeÃ§ar o projeto para decifrar o enigma, precisamos voltar no tempo e relembrar alguns conceitos que serÃ£o essenciais para decifrarmos os cÃ³digos...";
+		historia[1] = "VocÃª precisa nos provar que realmente estÃ¡ capacitado para esse projeto, com isso vamos comeÃ§ar relembrando algumas coisas do ensino mÃ©dio";
+		historia[2] = "Agora que vocÃª faz parte do projeto ultra secreto, vamos por a mÃ£o na massa!\r\n" + 
+				"Muitas vidas jÃ¡ foram perdidas com essa guerra, estamos correndo contra o tempo para salvar o maior nÃºmero de pessoas e vencer a guerra.\r\n" + 
+				"Temos que comeÃ§ar resolvendo essa primeira questÃ£o, vamos lÃ¡...\r\n" + 
 				"";
-		historia[3] = "*Agora faremos uma viagem para nossa base militar, os desafios se tornarão maiores e precisamos que você esteja concentrado para vencer...\r\n" + 
-				"A enigma funciona com diversos códigos que mudam a cada ataque do inimigo, conseguimos resgatar um, mas não temos a resposta, vejamos se você pode nos ajudar...\r\n" + 
+		historia[3] = "*Agora faremos uma viagem para nossa base militar, os desafios se tornarÃ£o maiores e precisamos que vocÃª esteja concentrado para vencer...\r\n" + 
+				"A enigma funciona com diversos cÃ³digos que mudam a cada ataque do inimigo, conseguimos resgatar um, mas nÃ£o temos a resposta, vejamos se vocÃª pode nos ajudar...\r\n" + 
 				"";
-		historia[4] = "Agora que estamos pertos do primeiro sucesso, surgiu um problema mais difícil, envolve  expressões equivalentes, seja o gênio que precisamos...\r\n" + 
+		historia[4] = "Agora que estamos pertos do primeiro sucesso, surgiu um problema mais difÃ­cil, envolve  expressÃµes equivalentes, seja o gÃªnio que precisamos...\r\n" + 
 				"Respira e vamos ao primeiro sucesso...\r\n" + 
 				"";
-		historia[5] = "*Mensagem da história 06*";
-		historia[6] = "*Mensagem da história 07*";
-		historia[7] = "*Mensagem da história 08*";
-		historia[8] = "*Mensagem da história 09*";
-		historia[9] = "*Mensagem da história 10*";
+		historia[5] = "*Mensagem da histÃ³ria 06*";
+		historia[6] = "*Mensagem da histÃ³ria 07*";
+		historia[7] = "*Mensagem da histÃ³ria 08*";
+		historia[8] = "*Mensagem da histÃ³ria 09*";
+		historia[9] = "*Mensagem da histÃ³ria 10*";
 
-		// Vetor com os enunciados das questões
-		enunciado[0] = "1)Qual é o dobro de dois elevado a décima oitava potência?\"";
-		enunciado[1] = "2)No sistema de numeração de base binária, como escrevemos o número 10?";
-		enunciado[2] = "3)Quais expressões são equivalentes a raiz[5](b^9)? ";
-		enunciado[3] = "4) - Quais expressões são equivalentes a (d^1/8)^5?";
-		enunciado[4] = "*Enunciado Pergunta 05*";
-		enunciado[5] = "*Enunciado Pergunta 06*";
-		enunciado[6] = "*Enunciado Pergunta 07*";
-		enunciado[7] = "*Enunciado Pergunta 08*";
-		enunciado[8] = "*Enunciado Pergunta 09*";
-		enunciado[9] = "*Enunciado Pergunta 10*";
+		// Vetor com os enunciados das questÃµes
+		enunciado[0] = "1) Qual Ã© o dobro de dois elevado a dÃ©cima oitava potÃªncia?\"";
+		enunciado[1] = "2) No sistema de numeraÃ§Ã£o de base binÃ¡ria, como escrevemos o nÃºmero 10?";
+		enunciado[2] = "3) Quais expressÃµes sÃ£o equivalentes a raiz[5](b^9)? ";
+		enunciado[3] = "4) Quais expressÃµes sÃ£o equivalentes a (d^1/8)^5?";
+		enunciado[4] = "5) Sua resposta deve ser um polinÃ´mio na forma padrÃ£o. ";
+		enunciado[5] = "6) Qual expressÃ£o exponencial Ã© equivalente a (^7a)^2 ?";
+		enunciado[6] = "7) Sua resposta deve ser um polinÃ´mio na forma padrÃ£o(x^2+4)(x^2âˆ’6)";
+		enunciado[7] = "8) Descubra o numero escondido por trÃ¡s do cÃ³digo abaixo: 3x â€“ 15 = 5x + 25";
+		enunciado[8] = "9) Descubra o numero escondido por trÃ¡s do cÃ³digo abaixo: 2.(3x-10) = 8x.(10-4)Â²";
+		enunciado[9] = "10) Decodifique as raÃ­zes do criptografo dos nazista: xÂ² - 10x â€“ 25 = 0";
 		
-		//Vetor com as alternativas das questões
+		//Vetor com as alternativas das questÃµes
 		
 		
-		alternativaA[0] = "a) Dois elevado a décima nona potência ";
+		alternativaA[0] = "a) Dois elevado a dÃ©cima nona potÃªncia ";
 		alternativaA[1] = "a) 1010";
 		alternativaA[2] = "a) (b^5)^1/9";
 		alternativaA[3] = "a) (d^5)^8";
-		alternativaA[4] = "a) Respota certa";
-		alternativaA[5] = "a) Respota certa";
-		alternativaA[6] = "a) Respota certa";
-		alternativaA[7] = "a) Respota certa";
-		alternativaA[8] = "a) Respota certa";
-		alternativaA[9] = "a) Respota certa";
+		alternativaA[4] = "a) -5^3 - 28y^2-33y+18";
+		alternativaA[5] = "a) a^7a^2";
+		alternativaA[6] = "a) x^4-2x^2-28";
+		alternativaA[7] = "a) 20";
+		alternativaA[8] = "a) -20";
+		alternativaA[9] = "a) -25 ; 5";
 		
-		alternativaB[0] = "b)Dois elevado a trigésima sexta potência";
-		alternativaB[1] = "b)11";
+		alternativaB[0] = "b) Dois elevado a trigÃ©sima sexta potÃªncia";
+		alternativaB[1] = "b) 11";
 		alternativaB[2] = "b) b^5/9";
 		alternativaB[3] = "b) (raiz[8](d))^5";
-		alternativaB[4] = "Alternativa B pergunta 05";
-		alternativaB[5] = "Alternativa B pergunta 06";
-		alternativaB[6] = "Alternativa B pergunta 07";
-		alternativaB[7] = "Alternativa B pergunta 08";
-		alternativaB[8] = "Alternativa B pergunta 09";
-		alternativaB[9] = "Alternativa B pergunta 10";
+		alternativaB[4] = "b) -5^3 - 28y^2-33y+17";
+		alternativaB[5] = "b) a^2a^7";
+		alternativaB[6] = "b) x^4-2x^2-22";
+		alternativaB[7] = "b) 15";
+		alternativaB[8] = "b) -10*";
+		alternativaB[9] = "b) -5;5*";
 		
-		alternativaC[0] = "c)Um elevado a décima sexta potência";
-		alternativaC[1] = "c)1001";
+		alternativaC[0] = "c) Um elevado a dÃ©cima sexta potÃªncia";
+		alternativaC[1] = "c) 1001";
 		alternativaC[2] = "c) b^9/5 ";
 		alternativaC[3] = "c) (d^5)^1/8";
-		alternativaC[4] = "Alternativa C pergunta 05";
-		alternativaC[5] = "Alternativa C pergunta 06";
-		alternativaC[6] = "Alternativa C pergunta 07";
-		alternativaC[7] = "Alternativa C pergunta 08";
-		alternativaC[8] = "Alternativa C pergunta 09";
-		alternativaC[9] = "Alternativa C pergunta 10";
+		alternativaC[4] = "c) -5^3 - 28y^2-33y+16";
+		alternativaC[5] = "c) 2|7";
+		alternativaC[6] = "c) x^4-2x^2-24";
+		alternativaC[7] = "c) 10";
+		alternativaC[8] = "c) -6";
+		alternativaC[9] = "c) 25;-10";
 		
-		alternativaD[0] = "d)Um elevado a trigésima nona potência";
-		alternativaD[1] = "d)22";
+		alternativaD[0] = "d) Um elevado a trigÃ©sima nona potÃªncia";
+		alternativaD[1] = "d) 22";
 		alternativaD[2] = "d) Nenhuma das anteriores";
 		alternativaD[3] = "d) Nenhuma das anteriores";
-		alternativaD[4] = "Alternativa D pergunta 05";
-		alternativaD[5] = "Alternativa D pergunta 06";
-		alternativaD[6] = "Alternativa D pergunta 07";
-		alternativaD[7] = "Alternativa D pergunta 08";
-		alternativaD[8] = "Alternativa D pergunta 09";
-		alternativaD[9] = "Alternativa D pergunta 10";
+		alternativaD[4] = "d) Nenhuma das anteriores";
+		alternativaD[5] = "d) Nenhuma das anteriores";
+		alternativaD[6] = "d) Nenhuma das anteriores";
+		alternativaD[7] = "d) 5";
+		alternativaD[8] = "d) 6";
+		alternativaD[9] = "d) -10 ; 10";
 		
-		//Vetores com as respostas postivas e negativa dependendo da resposta do usuário á questão
+		//Vetores com as respostas postivas e negativa dependendo da resposta do usuÃ¡rio Ã¡ questÃ£o
 		respostaPositiva[0] = "";
 		respostaPositiva[1] = "";
 		respostaPositiva[2] = "";
@@ -222,8 +222,8 @@ public class Main {
 
 		do {
 			System.out.println(
-					"\nBem-vindo ao Menu Principal! " + "\n 1 - Jogar" + "\n 2 - Instruções" + "\n 3 - Créditos"
-							+ "\n 4 - Sair" + "\nSelecione uma opção digitando por seu número equivalente:");
+					"\nBem-vindo ao Menu Principal! " + "\n 1 - Jogar" + "\n 2 - InstruÃ§Ãµes" + "\n 3 - CrÃ©ditos"
+							+ "\n 4 - Sair" + "\nSelecione uma opÃ§Ã£o digitando por seu nÃºmero equivalente:");
 
 			switch (entrada.nextInt()) {
 			case 1:
@@ -241,7 +241,7 @@ public class Main {
 				opSair();
 
 			default:
-				System.out.println("Opção Invalida! Digite Novamente");
+				System.out.println("OpÃ§Ã£o Invalida! Digite Novamente");
 				break;
 			}
 		} while (true);
@@ -249,7 +249,7 @@ public class Main {
 	}
 
 	static void opJogar() {
-		System.out.println("|---Opção Jogar---|");
+		System.out.println("|---OpÃ§Ã£o Jogar---|");
 		
 		String resposta;
 		boolean confimaResposta = false;
@@ -270,12 +270,12 @@ public class Main {
 				
 			do {
 				
-				 System.out.println("Digite a opção escolhida abaixo: ");
+				 System.out.println("Digite a opÃ§Ã£o escolhida abaixo: ");
 				 resposta = entrada.next();
 				 
 				switch (resposta) {
 				
-				// Variações de "A"
+				// VariaÃ§Ãµes de "A"
 				
 				case "a":
 					System.out.println("\n");
@@ -295,7 +295,7 @@ public class Main {
 					confimaResposta = true;
 					break;
 					
-					// Variações de "B"
+					// VariaÃ§Ãµes de "B"
 					
 				case "b":
 					System.out.println("\n");
@@ -314,7 +314,7 @@ public class Main {
 					confimaResposta = true;
 					break;
 					
-					// Variações de "C"
+					// VariaÃ§Ãµes de "C"
 					
 				case "c":
 					System.out.println("\n");
@@ -334,7 +334,7 @@ public class Main {
 					}
 					break;
 					
-					// Variações de "D"
+					// VariaÃ§Ãµes de "D"
 					
 				case "d":
 					System.out.println("\n");
@@ -367,23 +367,23 @@ public class Main {
 			
 		}
 		
-		System.out.println("Você acertou " + contPontos + " pontos");
+		System.out.println("VocÃª acertou " + contPontos + " pontos");
 		opSair();
 	}
 
 	static void opIntrucoes() {
-		System.out.println("|---Opção Instruções---|");
+		System.out.println("|---OpÃ§Ã£o InstruÃ§Ãµes---|");
 
 	}
 
 	static void opCreditos() {
-		System.out.println("|---Opção Créditos---|");
+		System.out.println("|---OpÃ§Ã£o CrÃ©ditos---|");
 
 	}
 
 	static void opSair() {
 		System.out.println("\nFim de Jogo.");
-		System.out.println("\nAté a proxíma!");
+		System.out.println("\nAtÃ© a proxÃ­ma!");
 		System.exit(0);
 	}
 
